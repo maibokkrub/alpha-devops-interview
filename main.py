b42d50183a97fd58e51aa8982dfe7c2ccc7db22e
maibokkrub <maibokkrub@github.com>
2021-12-15 07:57:19 +0000

    feat: bootstrap

---
+++ main.py
@@ -0,0 +1,14 @@
+import click
+
+def validate_configurations(): 
+    pass 
+
+def initialize_modules(): 
+    pass
+
+def main(): 
+    pass 
+
+if __name__ == '__main__':
+    validate_configurations()
+    main()
---
+++ modules/detail.py
@@ -0,0 +1,5 @@
+import web3
+
+def detail():
+    ''' Doc String '''
+    pass
