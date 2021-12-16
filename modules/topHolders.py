eff23005023141da9a1a2078fb35aacdd4edff4d
maibokkrub <maibokkrub@github.com>
2021-12-16 10:37:42 +0700

    feat: rewrite & improve top holders

--- modules/topHolders.py
+++ modules/topHolders.py
@@ -1,8 +1,9 @@
-from collections import Counter
-import enum
+
+import csv
 import click
 from time import sleep, time_ns
 from utils import fetch_contract_transfer_events, get_ERC20contract_instance
+import pandas as pd 
 
 @click.command()
 @click.argument('contract_address')
@@ -16,7 +17,7 @@ def topHolders(contract_address, amount):
     # click.echo(f"[INFO] Fetching latest {amount} transactions -- newest to oldest")
     click.echo("[INFO] Fetching Contract Data")
     contract = get_ERC20contract_instance(contract_address)
-    decimals    = 10 ** contract.functions.decimals().call()
+    decimals    = contract.functions.decimals().call()
     symbol      = contract.functions.symbol().call()
 
     holders = {}
@@ -29,7 +30,7 @@ def topHolders(contract_address, amount):
         for transfer in results:
             tx_from = transfer['from']
             tx_to   = transfer['to'] 
-            tx_val  = int(transfer['value']) / decimals
+            tx_val  = int(transfer['value'])
             if tx_from not in holders:
                 holders[tx_from] = 0 
             if tx_to not in holders: 
@@ -40,32 +41,20 @@ def topHolders(contract_address, amount):
         current_page += 1
         sleep(0.3) #Rate Limit
         results = fetch_contract_transfer_events(contract_address, page=1, startblock=int(results[-1]['blockNumber'])+1)
+
+    # For reading from csv
+    # with open(f'allholders-{contract_address}.csv', 'r') as csv_file:  
+    #     reader = csv.reader(csv_file)
+    #     for line in reader: 
+    #         holders[line[0]] = int(line[1])
+        
+    df = pd.DataFrame.from_dict(holders, orient='index', columns=[symbol])
+    df.reset_index(level=0, inplace=True)
+    df.rename(columns={'index':'Address'}, inplace=True)
+    df.to_csv(f'allholders-{contract_address}.csv')
+    top_holders = df.sort_values(by=[symbol], ascending=False)[:int(amount)]
+    top_holders[symbol] = top_holders[symbol] / 10**decimals 
     
-    counts_sorted = sorted(holders.values())
-
-    fp = open('topholders-'+contract_address,'w')
-    fp.writelines(holders)
-
-    amount = int(amount)
-    top_holders  = []
-    shown_count  = -1
-    prev = -1 
-    # round_winner = set()
-    for winning_value in reversed(counts_sorted):
-        if prev != winning_value:
-            if shown_count >= amount: 
-                break
-            winner = (key for key, value in holders.items() if value == winning_value)
-            top_holders.append((prev, [k for k in winner]))
-            prev = winning_value
-            # round_winner = set()
-        holders[winner] = -1
-        shown_count += 1
-
-    
-    for entry in top_holders[1:]: 
-        #Todo: cleanup to decimals
-        print(entry[0], symbol, )
-        for p in entry[1]: 
-            print(f'  {p}')
-
+    print(f"Top {amount} holders for {symbol}")
+    pd.options.display.float_format = ('{:.0'+str(decimals)+'f}').format
+    print(top_holders)
